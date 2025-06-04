75118e765d5d22dc3dc3bf48f831bdbcd51c2991
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-06-04 16:49:47 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import os
 import time
 import requests
+import logging
 from dotenv import load_dotenv
 from telegram import Bot
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, bot_token, chat_id):
+        super().__init__()
+        self.bot = Bot(token=bot_token)
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        try:
+            self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+        except Exception:
+            pass
 
 
 def send_notification(bot, chat_id, message):
@@ -18,6 +33,15 @@ def main():
 
     bot = Bot(token=bot_token)
 
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(message)s'
+    )
+    logger = logging.getLogger()
+    logger.addHandler(TelegramLogsHandler(bot_token, tg_chat_id))
+
+    logger.info('✅ Бот запущен!')
+
     headers = {
         'Authorization': f'Token {dvmn_token}',
     }
@@ -25,7 +49,12 @@ def main():
     params = {}
     while True:
         try:
-            response = requests.get('https://dvmn.org/api/long_polling/', params=params, headers=headers, timeout=90)
+            response = requests.get(
+                'https://dvmn.org/api/long_polling/',
+                params=params,
+                headers=headers,
+                timeout=90
+            )
             response.raise_for_status()
             reviews_response = response.json()
 
@@ -47,7 +76,7 @@ def main():
                     else:
                         message = (
                             f'У вас проверили работу "{lesson}"\n\n'
-                            f'Преподавателю все понравилось, можно приступать к следующему уроку!\n\n'
+                            f'Преподавателю всё понравилось, можно приступать к следующему уроку!\n\n'
                             f'Ссылка на урок: {lesson_url}'
                         )
                     send_notification(bot, tg_chat_id, message)
@@ -55,10 +84,14 @@ def main():
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
+            logger.warning('🔌 Потеря соединения. Ждём...')
             time.sleep(60)
             continue
-        except requests.exceptions.JSONDecodeError:
+        except requests.exceptions.JSONDecodeError as e:
+            logger.warning('⚠️ JSON ошибка: %s', e)
             continue
+        except Exception as e:
+            logger.exception('❗ Бот упал с ошибкой:')
 
 
 if __name__ == '__main__':
